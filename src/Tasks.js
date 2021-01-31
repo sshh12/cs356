f6ce40a383fe7183ab117f49b6ba3f0bd31afcd9
Shrivu Shankar <shrivu1122@gmail.com>
2021-01-31 13:16:53 -0600

    more tasks

--- src/Tasks.js
+++ src/Tasks.js
@@ -13,7 +13,41 @@ const TASKS = [
     defaultUnits={{ time: "second" }}
   />,
   <Task
-    title={"Max Users With Packet Switching"}
+    title={"Packet Switching, Queuing Delay"}
+    problem={
+      "A router that processes at $RATE with an average arrival of $AVGARRIVAL of $SIZE packets, will have a queuing delay of $QDELAY (which is $QSTATUS)."
+    }
+    vars={{
+      rate: "16 Mbps",
+      avgarrival: "1000 count/s",
+      size: "2000 bytes",
+      qdelay: null,
+      qstatus: null,
+    }}
+    calcs={{
+      qdelay: ({ rate, avgarrival, size }) => {
+        rate = Qty(rate);
+        avgarrival = Qty(avgarrival);
+        size = Qty(size);
+        return size.mul(avgarrival).div(rate).scalar;
+      },
+      qstatus: ({ rate, avgarrival, size }) => {
+        rate = Qty(rate);
+        avgarrival = Qty(avgarrival);
+        size = Qty(size);
+        let val = size.mul(avgarrival).div(rate).scalar;
+        if (val < 0.7) {
+          return "SMALL";
+        } else if (val <= 1) {
+          return "LARGE";
+        }
+        return "INFINITE";
+      },
+    }}
+    defaultUnits={{}}
+  />,
+  <Task
+    title={"Packet Switching, Max Users"}
     problem={
       "Given a $RATE total link and $P % active users that use $USERRATE with $AVAIL % availability, there can be $MAX users ($PEAKUSERS)."
     }
@@ -45,11 +79,39 @@ const TASKS = [
         }
         return found ? n - 1 : "";
       },
-      peakusers: ({ rate, p, userrate, avail }) => {
+      peakusers: ({ rate, userrate }) => {
         return Qty(rate).div(Qty(userrate)).scalar;
       },
     }}
     defaultUnits={{}}
+  />,
+  <Task
+    title={"Packet Switching, Transmission Delay"}
+    problem={
+      "If a $SIZE payload is sent over $RATE $HOPS as $PACKETS equal-sized packets (with $HEADER header size), it will take $TIME."
+    }
+    vars={{
+      size: "1MB",
+      rate: "20 Mbps",
+      hops: "3",
+      packets: "1000",
+      header: "100 bytes",
+      time: null,
+    }}
+    calcs={{
+      time: ({ size, rate, hops, packets, header }) => {
+        size = Qty(size);
+        rate = Qty(rate);
+        hops = parseInt(hops);
+        packets = parseInt(packets);
+        header = Qty(header);
+        let packetSize = size.div(packets).add(header);
+        let perHopTime = packetSize.div(rate);
+        let firstArrive = perHopTime.mul(hops);
+        return firstArrive.add(perHopTime.mul(packets - 1));
+      },
+    }}
+    defaultUnits={{ time: "second" }}
   />,
 ];
 
